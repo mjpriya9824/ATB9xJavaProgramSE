c795593e86b9c078d26a7bc56bf1743557d696f4
Jyothi Priya <mjpriya9824@gmail.com>
2024-10-20 18:40:36 +0530

    commit and push the code2

--- src/Task/task_03102024/Task2.java
+++ src/Task/task_03102024/Task2.java
@@ -1,7 +1,7 @@
 package Task.task_03102024;
 
 public class Task2 {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         System.out.println("10x1=10");
         System.out.println("10x2=20");
